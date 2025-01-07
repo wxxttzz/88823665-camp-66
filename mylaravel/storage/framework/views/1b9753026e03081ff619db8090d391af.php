c3acb8ebb17d63c6d7917e505ccf1835a4784e52
wxxttzz <changlixdhy@gmail.com>
2025-01-07 19:54:52 +0700

    Revert "some"
    This reverts commit 2aa24ee7d076624d1fca145ad0d8fbacaadeb3bc.

--- mylaravel/storage/framework/views/1b9753026e03081ff619db8090d391af.php
+++
@@ -1,9 +0,0 @@
-<h1>My View!</h1>
-<?php echo $value_id; ?>
-<?php echo e($value_id); ?>|<?php echo e($myinput); ?>
-
-<form action="<?php echo e(url('/mycontroller')); ?>" method="post">
-    <?php echo csrf_field(); ?>
-    <input type="text" name = "myinput">
-    <button type="submit">Submit</button>
-</form><?php /**PATH C:\xampp\htdocs\88823665-camp-66\mylaravel\resources\views/myView.blade.php ENDPATH**/ ?>
